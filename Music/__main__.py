9adeced20413e09d06ed7b681822d9c6ffb45426
JUSTME98 <balwindersingh91598@gmail.com>
2025-08-16 13:13:10 +0530

    ʀᴇɴᴅᴇʀ sᴇʀᴠᴇʀ

--- Music/__main__.py
+++ Music/__main__.py
@@ -1,7 +1,9 @@
 import asyncio
+import threading
+from flask import Flask, request
+from werkzeug.serving import make_server
 
 from pyrogram import idle
-
 from config import Config
 from Music.core.calls import PbxMusic
 from Music.core.clients import PbxBot
@@ -11,9 +13,35 @@ from Music.core.users import user_data
 from Music.helpers.strings import TEXTS
 from Music.version import __version__
 
+# ᴄʀᴇᴀᴛɪɴɢ ꜰʟᴀsᴋ ᴀᴘᴘ ꜰᴏʀ ʀᴇɴᴅᴇʀ ʜᴏsᴛɪɴɢ
+app = Flask(__name__)
+
+# ᴅᴇꜰɪɴɪɴɢ ʜᴏᴍᴇ ᴇɴᴅᴘᴏɪɴᴛ ꜰᴏʀ ʀᴇɴᴅᴇʀ
+@app.route('/')
+def home():
+    return "Bot is running"
+
+# ɢʟᴏʙᴀʟ ᴠᴀʀɪᴀʙʟᴇ ꜰᴏʀ ꜰʟᴀsᴋ ᴛʜʀᴇᴀᴅ
+flask_thread = None
+
+# sᴛᴀʀᴛɪɴɢ ꜰʟᴀsᴋ sᴇʀᴠᴇʀ ɪɴ ᴀ ᴛʜʀᴇᴀᴅ
+def run_flask():
+    # ᴜsᴇ 'ᴜsᴇ_ʀᴇʟᴏᴀᴅᴇʀ=ꜰᴀʟsᴇ' ᴛᴏ ᴀᴠᴏɪᴅ ᴍᴜʟᴛɪᴘʟᴇ ꜰʟᴀsᴋ sᴇʀᴠᴇʀs
+    port = int(os.environ.get("PORT", 8000))
+    LOGS.info(f"ʀᴇɴᴅᴇʀ sᴇʀᴠᴇʀ sᴛᴀʀᴛᴇᴅ ᴏɴ ᴘᴏʀᴛ: {port}")
+    app.run(host="0.0.0.0", port=port, use_reloader=False)
+
+# sʜᴜᴛᴅᴏᴡɴ ꜰᴜɴᴄᴛɪᴏɴ ꜰᴏʀ ꜰʟᴀsᴋ sᴇʀᴠᴇʀ
+def shutdown_flask():
+    # sᴇɴᴅs sʜᴜᴛᴅᴏᴡɴ sɪɢɴᴀʟ ᴛᴏ ꜰʟᴀsᴋ sᴇʀᴠᴇʀ
+    func = request.environ.get('werkzeug.server.shutdown')
+    if func:
+        func()
+    LOGS.info("ʀᴇɴᴅᴇʀ sᴇʀᴠᴇʀ sʜᴜᴛ ᴅᴏᴡɴ.")
 
 async def start_bot():
     
+    # ɢᴇᴛᴛɪɴɢ ᴠᴇʀsɪᴏɴ ɪɴꜰᴏ
     hmusic_version = __version__["Pbx Music"]
     py_version = __version__["Python"]
     pyro_version = __version__["Pyrogram"]
@@ -23,10 +51,16 @@ async def start_bot():
         "ᴀʟʟ ᴄʜᴇᴄᴋs ᴄᴏᴍᴘʟᴇᴛᴇᴅ! ʟᴇᴛ's sᴛᴀʀᴛ ᴘʙx-ᴍᴜsɪᴄ..."
     )
     
+    # sᴛᴀʀᴛɪɴɢ ᴜsᴇʀ ᴅᴀᴛᴀ, ʙᴏᴛ, ᴍᴜsɪᴄ, ᴀɴᴅ ᴅᴀᴛᴀʙᴀsᴇ
     await user_data.setup()
     await PbxBot.start()
     await PbxMusic.start()
     await db.connect()
+    
+    # sᴛᴀʀᴛɪɴɢ ʀᴇɴᴅᴇʀ sᴇʀᴠᴇʀ ɪɴ ᴀ sᴇᴘᴀʀᴀᴛᴇ ᴛʜʀᴇᴀᴅ
+    global flask_thread
+    flask_thread = threading.Thread(target=run_flask, daemon=True)
+    flask_thread.start()
     
     try:
         if Config.BOT_PIC:
@@ -65,8 +99,10 @@ async def start_bot():
         f">> ᴘʙx-ᴍᴜsɪᴄ [{hmusic_version}] ɪs ɴᴏᴡ ᴏɴʟɪɴᴇ!"
     )
     
+    # ᴋᴇᴇᴘɪɴɢ ʙᴏᴛ ᴀʟɪᴠᴇ ᴡɪᴛʜ ɪᴅʟᴇ
     await idle()
     
+    # sᴇɴᴅɪɴɢ sᴛᴏᴘ ᴍᴇssᴀɢᴇ
     await PbxBot.app.send_message(
         Config.LOGGER_ID,
         f"#sᴛᴏᴘ\n\n**ᴘʙx-ᴍᴜsɪᴄ [{hmusic_version}] ɪs ɴᴏᴡ ᴏꜰꜰʟɪɴᴇ!**",
@@ -76,14 +112,16 @@ async def start_bot():
         f"ᴘʙx-ᴍᴜsɪᴄ [{hmusic_version}] ɪs ɴᴏᴡ ᴏꜰꜰʟɪɴᴇ!"
     )
 
-
 if __name__ == "__main__":
     
+    # ʀᴜɴɴɪɴɢ ᴍᴀɪɴ ᴇᴠᴇɴᴛ ʟᴏᴏᴘ
     loop = asyncio.get_event_loop()
     
     try:
         loop.run_until_complete(start_bot())
     
     finally:
+        # sʜᴜᴛᴛɪɴɢ ᴛʜᴇ ᴅᴏᴡɴ ᴛʜᴇ ꜰʟᴀsᴋ sᴇʀᴠᴇʀ
+        shutdown_flask()
         loop.run_until_complete(PbxBot.app.stop())
         loop.close()
